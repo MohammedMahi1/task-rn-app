4b73671d583d6b69891a060313cae6c5a8c56d7a
Mohammed Mahi <mohammed.mahi012@gmail.com>
2025-09-29 20:45:31 +0100

    fix: modal screen

--- src/screens/modals/TaskModal.tsx
+++ src/screens/modals/TaskModal.tsx
@@ -1,21 +1,46 @@
-import { View, Text, Touchable, TouchableWithoutFeedback, Pressable, TouchableOpacity } from 'react-native'
-import React from 'react'
-import Container from '../../components/Container'
-import Box from '../../components/ui/Box'
-import { useAppSelector } from '../../hooks/state'
-import { useNavigation } from '@react-navigation/native'
+import {
+  View,
+  Text,
+  Touchable,
+  TouchableWithoutFeedback,
+  Pressable,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import Container from "../../components/Container";
+import Box from "../../components/ui/Box";
+import { useAppSelector } from "../../hooks/state";
+import { useNavigation } from "@react-navigation/native";
 
 const TaskModal = () => {
-    const {selectedDate,selectedDay} = useAppSelector(state=>state.date)
-    const navigate = useNavigation()
+  const { selectedDate, selectedDay } = useAppSelector((state) => state.date);
+  const navigate = useNavigation<any>();
   return (
-    <Pressable style={{flex:1,width:"100%",flexDirection:"column",justifyContent:"flex-end",backgroundColor:"red",zIndex:99,position:"relative"}} onPress={()=>navigate.goBack()}>
-        <Box pointerEvents='none' style={{padding:24,backgroundColor:"white",height:"60%",width:"100%",borderTopLeftRadius:16,position:"absolute",zIndex:0,borderTopRightRadius:16,boxShadow:"0px -1px 60px -20px rgba(0,0,0,0.1)",elevation:5}}>
+    <Pressable
+      style={{
+        flex: 1,
+        width: "100%",
+        flexDirection: "column",
+        justifyContent: "flex-end",
+      }}
+      onPress={() => navigate.goBack()}
+    >
+      <Pressable
+        style={{
+          padding: 24,
+          backgroundColor: "white",
+          height: "60%",
+          width: "100%",
+          borderTopLeftRadius: 16,
+          borderTopRightRadius: 16,
+          boxShadow: "0px -1px 60px -20px rgba(0,0,0,0.1)",
+        }}
+      >
         <Text>{selectedDay}</Text>
         <Text>{selectedDate}</Text>
-        </Box>
-    </Pressable >
-  )
-}
+      </Pressable>
+    </Pressable>
+  );
+};
 
-export default TaskModal
+export default TaskModal;
